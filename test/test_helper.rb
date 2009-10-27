57b06cf30573f224bcdc8c62f8bd44940d64260a
Yuya Kato <yuyakato@gmail.com>
2009-10-27 14:47:02 +0900

    redgreenを導入

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,22 @@
 #==============================================================================#
 
 require "test/unit"
-require File.dirname(__FILE__) + "/../lib/kagemusha"
+require "rubygems"
+
+begin
+  require "redgreen"
+rescue LoadError
+  # nop
+end
+
+begin
+  require "win32console" if /win32/ =~ RUBY_PLATFORM
+rescue LoadError
+  # nop
+end
+
+$:.unshift(File.dirname(__FILE__) + "/../lib")
+require "kagemusha"
 
 #==============================================================================#
 #==============================================================================#
